061088a714e2bc2d8caab35de817fb0f250ee3eb
Laptop <julian.flores.armijos@udla.edu.ec>
2023-10-01 18:09:56 +0200

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_Version_Final.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int cantidad {  get; set; }
+    }
+}
---
+++ Util/Utils.cs
@@ -0,0 +1,25 @@
+﻿using CRUD_Version_Final.Models;
+
+namespace CRUD_Version_Final.Util
+{
+    public class Utils
+    {
+        public static List<Producto> ListaProducto = new List<Producto>()
+        {
+            new Producto()
+            {
+                IdProducto = 1,
+                Nombre= "Producto 1",
+                Descripcion="Descripcion",
+                cantidad=1
+            },
+            new Producto()
+            {
+                IdProducto = 2,
+                Nombre= "Producto 2",
+                Descripcion="Descripcion",
+                cantidad=2
+            }
+        };
+    }
+}
